546678f3402fc39eda46575c107eb0adb0651f05
Gabriel Nunes Muller <gabriel.218118@alunosatc.edu.br>
2026-05-18 21:03:30 -0300

    Create: Initial Setup

---
+++ ContaBancaria/Template/Servicos/Conta.cs
@@ -0,0 +1,29 @@
+﻿namespace ContaBancaria
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+
+        public string Titular { get; set; }
+
+        public string NumeroConta {  get; set; }
+
+        public EnumTipoConta TipoConta { get; set; }
+
+        public decimal SaldoConta { get; set; }
+
+        public bool Ativo {  get; set; }
+    }
+
+    public enum EnumTipoConta
+    {
+        ContaCorrente = 1,
+        Pupança = 2
+    }
+
+    public enum EnumTipoMovimentacao
+    {
+        Entrada = 1,
+        Saida = 2
+    }
+}
